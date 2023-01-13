54ab20d16a50fb78c246c532b30fb8ae52c28a56
hellboy41785 <unknown41785@gmail.com>
2023-01-13 10:14:05 +0530

    modified:   .gitignore 	new file:   components/ChartDatas.js 	modified:   package-lock.json 	modified:   package.json 	modified:   pages/charts.js

---
+++ components/ChartDatas.js
@@ -0,0 +1,136 @@
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import dynamic from "next/dynamic";
+const Charts = dynamic(() => import("react-apexcharts"), { ssr: false });
+
+const ChartData = ({ chart, name }) => {
+  const formatData = (data) => {
+    return data.t.map((el, index) => {
+      const date = new Date(el * 1000);
+      return {
+        x: date.toLocaleString("en-us", {
+          day: "numeric",
+          month: "short",
+          hour: "2-digit",
+          minute: "2-digit",
+        }),
+        y: data.i[index],
+      };
+    });
+  };
+  const formatData1 = (data) => {
+    return data.t.map((el, index) => {
+      const date = new Date(el * 1000);
+      return {
+        x: date.toLocaleString("en-us", {
+          day: "numeric",
+          month: "short",
+          hour: "2-digit",
+          minute: "2-digit",
+        }),
+        y: data.w[index],
+      };
+    });
+  };
+  const formatData2 = (data) => {
+    return data.t.map((el, index) => {
+      const date = new Date(el * 1000);
+      return {
+        x: date.toLocaleString("en-us", {
+          day: "numeric",
+          month: "short",
+          hour: "2-digit",
+          minute: "2-digit",
+        }),
+        y: data.v[index],
+      };
+    });
+  };
+  const formatData3 = (data) => {
+    return data.t.map((el, index) => {
+      const date = new Date(el * 1000);
+      return {
+        x: date.toLocaleString("en-us", {
+          day: "numeric",
+          month: "short",
+          hour: "2-digit",
+          minute: "2-digit",
+        }),
+        y: data.wp[index],
+      };
+    });
+  };
+
+  const myChartData = {
+    intraDa: formatData(chart),
+    weightedIntra: formatData1(chart),
+    volume: formatData2(chart),
+    weightedPCR: formatData3(chart),
+  };
+  const { intraDa, weightedIntra, volume, weightedPCR } = myChartData;
+  const options = {
+    title: {
+      text: name,
+      align: "center",
+      style: {
+        fontSize: "24px",
+      },
+    },
+    stroke: {
+      curve: "smooth",
+    },
+    chart: {
+      id: "stock data",
+    },
+    stroke: {
+      width: 3,
+    },
+    xaxis: {
+      tickAmount: 0,
+      tickInterval: 0.5,
+    },
+    yaxis: {
+      forceNiceScale: true,
+      decimalsInFloat: 1,
+      tickAmount: 8,
+    },
+  };
+
+  const series = [
+    {
+      name: "IP",
+      data: intraDa,
+    },
+    {
+      name: "WIP",
+      data: weightedIntra,
+    },
+    {
+      name: "VWP",
+      data: volume,
+    },
+    {
+      name: "WP",
+      data: weightedPCR,
+    },
+  ];
+  return (
+    <Charts
+      options={options}
+      series={series}
+      type="line"
+      width={1000}
+      height={650}
+    />
+  );
+};
+
+export default ChartData;
--- pages/charts.js
+++ pages/charts.js
@@ -1,12 +1,11 @@
-// import Chart from 'react-apexcharts'
 import { useOptionChainStore } from "../query/useOptionChainStore";
-import dynamic from "next/dynamic";
-import { useStockDataQuery, fetchStockData } from "../query/useStockData";
+import { useStockDataQuery} from "../query/useStockData";
 import goBack from "../public/goBack.png";
 import Image from "next/image";
 import Link from "next/link";
 import Loader from "../components/Loader";
-const MyCharts = dynamic(() => import("react-apexcharts"), { ssr: false });
+import ChartData from "../components/ChartDatas";
+
 
 const Charts = () => {
   const contracts = useOptionChainStore((state) => state.contracts);
@@ -17,127 +16,13 @@ const Charts = () => {
   if (isLoading) return <Loader />;
 
   const chart = data[0];
-  // console.log(chart)
-
-  const formatData = (data) => {
-    return data.t.map((el, index) => {
-      const date = new Date(el * 1000);
-      return {
-        x: date.toLocaleTimeString("en-US", {
-          day: "numeric",
-          month: "short",
-          hour: "2-digit",
-          minute: "2-digit",
-        }),
-        y: data.i[index],
-      };
-    });
-  };
-  const formatData1 = (data) => {
-    return data.t.map((el, index) => {
-      const date = new Date(el * 1000);
-      return {
-        x: date.toLocaleTimeString("en-US", {
-        day: "numeric",
-        month: "short",
-        hour: "2-digit",
-        minute: "2-digit"
-    }),
-        y: data.w[index],
-      };
-    });
-  };
-  const formatData2 = (data) => {
-    return data.t.map((el, index) => {
-      const date = new Date(el * 1000);
-      return {
-        x: date.toLocaleTimeString("en-US", {
-        day: "numeric",
-        month: "short",
-        hour: "2-digit",
-        minute: "2-digit"
-    }),
-        y: data.v[index],
-      };
-    });
-  };
-  const formatData3 = (data) => {
-    return data.t.map((el, index) => {
-      const date = new Date(el * 1000);
-      return {
-        x: date.toLocaleTimeString("en-US", {
-        day: "numeric",
-        month: "short",
-        hour: "2-digit",
-        minute: "2-digit"
-    }),
-        y: data.wp[index],
-      };
-    });
-  };
-
-  const myChartData = {
-    intraDa: formatData(chart),
-    weightedIntra: formatData1(chart),
-    volume: formatData2(chart),
-    weightedPCR: formatData3(chart),
-  };
-  const { intraDa, weightedIntra, volume, weightedPCR } = myChartData;
-  const options = {
-    title: {
-      text: contracts,
-      align: "center",
-      style: {
-        fontSize: "24px",
-      },
-    },
-    stroke: {
-      curve: "smooth",
-    },
-    markers: {
-      size: 4,
-    },
-    chart: {
-      id: "stock data",
-      animations: {
-        speed: 1300,
-      },
-    },
-    // xaxis: {
-    //   type : 'datetime'
-    // },
-    yaxis: {
-      forceNiceScale: true,
-      decimalsInFloat: 1,
-      tickAmount: 8,
-      tickInterval: 0.5
-    },
-  };
-
-  const series = [
-    {
-      name: "IP",
-      data: intraDa,
-    },
-    {
-      name: "WIP",
-      data: weightedIntra,
-    },
-    {
-      name: "VWP",
-      data: volume,
-    },
-    {
-      name: "WP",
-      data: weightedPCR,
-    },
-  ];
 
   const handleChange = (event) => {
     event.preventDefault();
     addContracts(event.target.value);
   };
 
+  // console.log(chart);
   return (
     <div className="p-3 ">
       <div className="flex justify-between ">
@@ -160,19 +45,11 @@ const Charts = () => {
           {/* <option>FINNIFTY</option> */}
         </select>
       </div>
-      <div className=" flex justify-center mt-7">
-        <MyCharts
-          options={options}
-          series={series}
-          type="line"
-          width={1200}
-          height={650}
-        />
+      <div className="flex justify-center items-center px-20">
+        <ChartData chart={chart} name={contracts}/>
       </div>
     </div>
   );
 };
 
 export default Charts;
-
-
