40b2eb2c5519e752e639cb694a11a9d33e720baa
hellboy41785 <unknown41785@gmail.com>
2023-02-22 14:14:20 +0530

    modified:   query/useOptionChain.js

--- query/useOptionChain.js
+++ query/useOptionChain.js
@@ -3,20 +3,16 @@ import { useQuery } from "react-query";
 
 
 
-
 export const fetchData = async (value) => {
-  
   const response = await axios.get(
-    `https://www.nseindia.com/api/option-chain-indices?symbol=${value}`
+    `http://localhost:5000/corsbypass?url=https://www.nseindia.com/api/option-chain-indices?symbol=${value}`
   );
   const data = response.data;
   return data;
 };
 
 export const useOptionChain = (value) => {
-  return useQuery(["option-chain",value], () => fetchData(value),
-  {
-    refetchIntervalInBackground: 120000
-  }
-  );
+  return useQuery(["option-chain", value], () => fetchData(value), {
+    refetchIntervalInBackground: 120000,
+  });
 };
